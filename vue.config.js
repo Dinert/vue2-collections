eff30190b34bed2e10cae2c347bcecfd794e66df
dinert <1293437362@qq.com>
2024-02-28 11:42:49 +0800

    feat: 增加打包线程，优化打包速度

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
 const path = require('path')
+const HappyPack = require('happypack')
 
 function resolve(dir) {
     return path.join(__dirname, dir)
@@ -47,52 +48,61 @@ module.exports = defineConfig({
 
         config.module.rule('svg').exclude.add(resolve('src/assets/icons')).end()
 
+        config.plugin('happypack').use(HappyPack, [{
+            id: 'happybabel',
+            loaders: ['babel-loader?cacheDirectory'],
+            // 开启 4 个线程
+            threads: 4
+        }])
+
+
         config.module
             .rule('icons')
             .test(/\.svg$/)
             .include.add(resolve('src/assets/icons'))
             .end()
             .use('svg-sprite-loader')
-            .loader('svg-sprite-loader')
+            .loader('svg-sprite-loader?id=happybabel')
             .options({
                 symbolId: 'icon-[name]',
             })
             .end()
 
-        config.when(process.env.NODE_ENV !== 'development', config => {
-            config
-                .optimization.splitChunks({
-                    chunks: 'all',
-                    cacheGroups: {
-                        libs: {
-                            name: 'chunk-libs',
-                            test: /[\\/]node_modules[\\/]/,
-                            priority: 10,
-                            chunks: 'initial' // only package third parties that are initially dependent
-                        },
-                        elementUI: {
-                            name: 'chunk-elementUI', // split elementUI into a single package
-                            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                        },
-                        commonsBaseUi: {
-                            name: 'chunk-commons-base-ui',
-                            test: resolve('src/base-ui'), // can customize your rules
-                            minChunks: 3, //  minimum common number
-                            priority: 5,
-                            reuseExistingChunk: true
-                        },
-                        commonsComponents: {
-                            name: 'chunk-commons-Components',
-                            test: resolve('src/components'), // can customize your rules
-                            minChunks: 3, //  minimum common number
-                            priority: 3,
-                            reuseExistingChunk: true
+        config.when(process.env.NODE_ENV !== 'development',
+            config => {
+                config
+                    .optimization.splitChunks({
+                        chunks: 'all',
+                        cacheGroups: {
+                            libs: {
+                                name: 'chunk-libs',
+                                test: /[\\/]node_modules[\\/]/,
+                                priority: 10,
+                                chunks: 'initial' // only package third parties that are initially dependent
+                            },
+                            elementUI: {
+                                name: 'chunk-elementUI', // split elementUI into a single package
+                                priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                            },
+                            commonsBaseUi: {
+                                name: 'chunk-commons-base-ui',
+                                test: resolve('src/base-ui'), // can customize your rules
+                                minChunks: 3, //  minimum common number
+                                priority: 5,
+                                reuseExistingChunk: true
+                            },
+                            commonsComponents: {
+                                name: 'chunk-commons-Components',
+                                test: resolve('src/components'), // can customize your rules
+                                minChunks: 3, //  minimum common number
+                                priority: 3,
+                                reuseExistingChunk: true
+                            }
                         }
-                    }
-                })
-            // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
-            config.optimization.runtimeChunk('single')
-        })
+                    })
+                // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+                config.optimization.runtimeChunk('single')
+            })
     },
 })
